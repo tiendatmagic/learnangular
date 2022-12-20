87b802363ed34ed2868748e76eed24c62d7ac4ce
tiendatmagic <tiendatmagic8@gmail.com>
2022-12-20 22:56:46 +0700

    update

--- src/app/combobox/combobox.component.ts
+++ src/app/combobox/combobox.component.ts
@@ -6,18 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./combobox.component.scss']
 })
 export class ComboboxComponent {
-  public districts: any = [];
-  public cities = [{
-    "city": "An Giang",
-    "district": ["a", "b", "c", "d", "e"]
-  }, {
-    "city": "Khánh Hòa",
-    "district": ["z", "y", "x", "m", "s"]
-  }];
-  changeCity(event: any) {
-    console.log(event.target.value);
+  public districts: any = ["Chọn quận huyện"];
+  public cities = [
+    {
+      "city": "Vui lòng chọn thành phố",
+      "district": ["Chọn quận huyện"]
+    },
+    {
+      "city": "An Giang",
+      "district": ["a", "b", "c", "d", "e"]
+    }, {
+      "city": "Khánh Hòa",
+      "district": ["z", "y", "x", "m", "s"]
+    }];
+  changeCity(event: any): void {
+    if (!event.target.value) {
+      return;
+    }
     const search = this.cities.filter((data) => data.city == event.target.value)
-    console.log(search);
-    this.districts = search[0].district;
+    if (search && search.length > 0) {
+      this.districts = search[0].district;
+    }
   }
 }
